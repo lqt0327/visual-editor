81e70f57385124716887e912faf58272147df0b8
lqt0327 <1485215848@qq.com>
2022-02-14 10:16:58 +0800

    chore: 增加文字颜色选择器

--- src/baseUI/leftPanel/editComponents/shared/title/index.jsx
+++ src/baseUI/leftPanel/editComponents/shared/title/index.jsx
@@ -1,7 +1,8 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Input } from 'antd';
 import _ from 'lodash'
 import CommonHoc from '../../common';
+import ColorSelect from '../../../../../components/color-select';
 
 const TitlePanel = (props) => {
   const { 
@@ -10,11 +11,21 @@ const TitlePanel = (props) => {
   } = props
 
   const path = useRef([])
+  const [color, setColor] = useState("#aabbcc");
+
+  useEffect(()=>{
+    console.log(color,'???======')
+    changeVal(path.current, color, "color")
+  },[color])
 
     return (
         <div className="schema-editor-container">
             <h3>标题</h3>
             <Input key={tpl["title"]} defaultValue={tpl["title"]} onChange={_.debounce((e) => changeVal(path.current, e.target.value, "title"), 250)} />
+            <h3>样式</h3>
+            <div className="editor-container-wrapper">
+              <ColorSelect updateColor={setColor} color={color} />
+            </div>
         </div>
     )
 }
---
+++ src/components/color-select/index.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { HexColorPicker } from "react-colorful";
+import './style.scss';
+
+const ColorSelect = (props) => {
+  const { updateColor, color } = props
+
+  const [flag, setFlag] = useState(false)
+
+  /**
+   * 展示颜色选择器
+   * @param {*} e 
+   */
+  const showColorEditor = (e) => {
+    // 阻止冒泡
+    e.nativeEvent.stopImmediatePropagation()
+    setFlag(true)
+  }
+
+  /**
+   * 点击空白区域关闭颜色选择器
+   */
+  document.addEventListener('click', () => {
+    setFlag(false)
+  })
+
+  return (
+    <div className="color-select">
+      <span>文字颜色：</span>
+      <div className="color-select-container" onClick={showColorEditor}>
+        <span className='color-select-editor'>{color} <div style={{"backgroundColor": color}}></div> </span>
+          {
+            flag ?
+            <HexColorPicker color={color} onChange={updateColor} className="color-picker" /> :
+            ''
+          } 
+      </div>
+    </div>
+    
+  )
+}
+
+export default ColorSelect
